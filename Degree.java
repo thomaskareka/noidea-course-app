f8a7ba55b56d0635b0017b7c5e06a3831068b736
mearacox <mearacox@gmail.com>
2024-02-22 14:30:02 -0500

    Completed #10

---
+++ Degree.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Degree {
+    private String major;
+    private String minor;
+    private String title;
+    private ArrayList<DegreeRequirement> requirements;
+    private int requiredCredits;
+
+    public Degree(String major, String minor, String title, ArrayList<DegreeRequirements> requirements,
+            int requiredCredits) {
+
+    }
+
+    public Degree(String major, String title, ArrayList<DegreeRequirements> requirements) {
+
+    }
+}
