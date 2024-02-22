9ce0e4d447cc2fd4972b4dacec51fc84a300e882
mearacox <mearacox@gmail.com>
2024-02-22 12:31:32 -0500

    Completed #13

---
+++ CourseProgress.java
@@ -0,0 +1,28 @@
+public class CourseProgress {
+    private Course course;
+    private Grade gradeReceived;
+    private String term;
+    private boolean isComplete;
+    private boolean isInProgress;
+
+    public CourseProgress(Course course, Grade gradeReceived, boolean isComplete, boolean isInProgress) {
+
+    }
+
+    public CourseProgress(Course course) {
+
+    }
+
+    public void makeCourseComplete() {
+
+    }
+
+    public void makeCourseInProgress() {
+
+    }
+
+    public void makeCourseUncomplete() {
+        
+    }
+
+}
