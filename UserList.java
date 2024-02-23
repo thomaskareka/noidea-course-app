358068e21467c3310f6a316e1cfe451a976739c9
Carolina Turner <carolinanturner@gmail.com>
2024-02-23 09:04:38 -0500

    addining #21

---
+++ UserList.java
@@ -0,0 +1,19 @@
+//#21
+
+import java.util.ArrayList;
+
+public class UserList {
+    private static UserList userlist;
+    private ArrayList<User> users;
+
+    private UserList(){
+
+    }
+    public UserList getInstance(){
+        return null;
+    }
+    public User getUser(String id){
+        return null;
+    }
+    
+}
