364dcd467317a2543308ae088b5c493f151b1ac7
Thomas Kareka <tkareka@email.sc.edu>
2024-02-23 22:17:09 -0500

    #18, extract course data from UofSC website

--- DataConstants.java
+++ DataConstants.java
@@ -1,4 +1,19 @@
 public abstract class DataConstants {
-    protected static final String USER_FILE_NAME = "json/users.json";
+    //protected static final String = "";
+
+    protected static final String ADVISOR_FILE_NAME = "json/advisors.json";
+    protected static final String COURSE_FILE_NAME = "json/courses.json";
+    protected static final String DEGREE_FILE_NAME = "json/degrees.json";
+    protected static final String STUDENT_FILE_NAME = "json/students.json";
+
     protected static final String UUID = "uuid";
+
+    protected static final String COURSE_NAME = "name";
+    protected static final String COURSE_ID = "identifier";
+    protected static final String COURSE_CREDITS = "credits";
+    protected static final String COURSE_DESCRIPTION = "description";
+    protected static final String COURSE_ATTRIBUTES = "attributes";
+    protected static final String COURSE_RESQUISITES = "requisites";
+    protected static final String COURSE_REQUISITES_TEXT = "requisiteText";
+
 }
