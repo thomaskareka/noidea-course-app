4bb8c0e2da9b34b75a0c7c0c6707cec1e9d77af9
awybranski <awybranski@gmail.com>
2024-02-25 14:56:09 -0500

    completed #26

--- User.java
+++ User.java
@@ -7,6 +7,7 @@ public abstract class User {
     protected String email;
     protected UUID id; 
 
+    // constuctor for new User
     public User(String firstName, String lastName, String email){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -14,10 +15,27 @@ public abstract class User {
         id = new UUID(18, 18);
     }
     
+    // constructor for loading users from JSON files
     public User(String firstName, String lastName, String email, UUID id){
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.id = id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public UUID getID(){
+        return id;
     }
 }
