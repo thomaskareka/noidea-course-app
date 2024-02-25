9a1fc02df04f560fc9b61e25b5dc72a38034a2ee
awybranski <awybranski@gmail.com>
2024-02-25 14:47:37 -0500

    completed #52

---
+++ UI.java
@@ -0,0 +1,14 @@
+public class UI {
+
+    public UI(){
+    }
+
+    public void run(){
+
+    }
+
+    public static void main(String[] args){
+        UI ui = new UI();
+        ui.run();
+    }
+}
