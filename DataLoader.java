6f1585ebb8e16c59edd7455c6391fb1d00899428
hudsondavis13 <hjd2@email.sc.edu>
2024-02-25 18:58:44 -0500

    #18, getStudents started

--- DataLoader.java
+++ DataLoader.java
@@ -8,7 +8,43 @@ import org.json.simple.parser.JSONParser;;
 
 public class DataLoader {
     public ArrayList<Student> getStudents() {
-        return new ArrayList<Student>();
+        ArrayList<Student> students = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+
+        try {
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("students.json"))
+            for (Object obj : jsonArray) {
+                JSONObject studentJson = (JSONObject) obj;
+
+                String firstName = (String) studentJson.get("firstName");
+                String lastName = (String) studentJson.get("lastName");
+                String email = (String) studentJson.get("email");
+                UUID id = UUID.fromString((String) studentJson.get("id"));
+                String major = (String) studentJson.get("major");
+                String minor = (String) studentJson.get("minor");
+                double majorGPA = (double) studentJson.get("majorGPA");
+                double overallGPA = (double) studentJson.get("overallGPA");
+                String classLevel = (String) studentJson.get("classLevel");
+                
+                JSONObject advisorJson = (JSONObject) studentJson.get("advisor");
+                Advisor advisor = new Advisor(firstName, lastName, email, id, students. isAdmin);
+                
+                boolean failureRisk = (boolean) studentJson.get("failureRisk");
+                ArrayList<String> notes = new ArrayList<>((JSONArray) studentJson.get("notes"));
+                boolean hasScholarship = (boolean) studentJson.get("hasScholarship");
+                
+                JSONObject degreeProgressJson = (JSONObject) studentJson.get("degreeProgress");
+                DegreeTracker degreeProgress = new DegreeTracker();
+                
+                Student student = new Student(firstName, lastName, email, id, major, minor, majorGPA, overallGPA, classLevel, advisor, failureRisk, notes, hasScholarship, degreeProgress);
+                students.add(student);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return students;
     }
 
     public ArrayList<Advisor> getAdvisors(){
