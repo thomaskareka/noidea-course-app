8eb3975f1325bc70c045fb04399e018be7e89508
Thomas Kareka <tkareka@email.sc.edu>
2024-02-25 20:20:20 -0500

    #42, rewrite to use data constants rather than hardcoded strings

--- DataLoader.java
+++ DataLoader.java
@@ -6,39 +6,41 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;;
 
-public class DataLoader {
+public class DataLoader extends DataConstants {
     public ArrayList<Student> getStudents() {
-        ArrayList<Student> students = new ArrayList<>();
-        JSONParser parser = new JSONParser();
+        ArrayList<Student> students = new ArrayList<Student>();
 
         try {
-            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("students.json"))
-            for (Object obj : jsonArray) {
-                JSONObject studentJson = (JSONObject) obj;
+            FileReader reader = new FileReader(STUDENT_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray studentList = (JSONArray) new JSONParser().parse(reader);
 
-                String firstName = (String) studentJson.get("firstName");
-                String lastName = (String) studentJson.get("lastName");
-                String email = (String) studentJson.get("email");
-                UUID id = UUID.fromString((String) studentJson.get("id"));
-                String major = (String) studentJson.get("major");
-                String minor = (String) studentJson.get("minor");
-                double majorGPA = (double) studentJson.get("majorGPA");
-                double overallGPA = (double) studentJson.get("overallGPA");
-                String classLevel = (String) studentJson.get("classLevel");
-                
-                UUID advisorID = UUID.fromString((String) studentJson.get("id"));
-                
-                boolean failureRisk = (boolean) studentJson.get("failureRisk");
-                ArrayList<String> notes = new ArrayList<>((JSONArray) studentJson.get("notes"));
-                boolean hasScholarship = (boolean) studentJson.get("hasScholarship");
-                
-                JSONObject degreeProgressJson = (JSONObject) studentJson.get("degreeProgress");
+            for(int i = 0; i < studentList.size(); i++) {
+                JSONObject studentJSON = (JSONObject)studentList.get(i);
+
+                UUID id = UUID.fromString((String)studentJSON.get(USER_ID));
+                String firstName = (String)studentJSON.get(USER_FIRST_NAME);
+                String lastName = (String)studentJSON.get(USER_LAST_NAME);
+                String email = (String)studentJSON.get(USER_EMAIL);
+
+                String major = (String)studentJSON.get(STUDENT_MAJOR);
+                String minor = (String)studentJSON.get(STUDENT_MINOR);
+                double majorGPA = (double)studentJSON.get(STUDENT_MAJOR_GPA);
+                double overallGPA = (double)studentJSON.get(STUDENT_GPA);
+                String classLevel = (String)studentJSON.get(STUDENT_CLASS);
+
+                UUID advisor = UUID.fromString((String)studentJSON.get(STUDENT_ADVISOR_ID));
+
+                boolean failureRisk = (boolean)studentJSON.get(STUDENT_AT_RISK);
+                ArrayList<String> notes = new ArrayList<String>((JSONArray) studentJSON.get(STUDENT_NOTES));
+                boolean hasScholarship = (boolean)studentJSON.get(STUDENT_SCHOLARSHIP);
+
+                JSONObject degreeJSON = (JSONObject) studentJSON.get(STUDENT_COURSE_LIST);
+                //TODO: add degree tracker inilization from student info
                 DegreeTracker degreeProgress = new DegreeTracker();
-                
-                Student student = new Student(firstName, lastName, email, id, major, minor, majorGPA, overallGPA, classLevel, advisorID, failureRisk, notes, hasScholarship, degreeProgress);
-                students.add(student);
+                Student s = new Student(firstName, lastName, email, id, major, minor, majorGPA, overallGPA, classLevel, advisor, failureRisk, notes, hasScholarship, degreeProgress);
+                students.add(s);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
