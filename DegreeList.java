be970562c969eeaf9f4aed1529ee76f71a9e966f
hudsondavis13 <hjd2@email.sc.edu>
2024-02-27 12:50:24 -0500

    updated #8

--- DegreeList.java
+++ DegreeList.java
@@ -15,4 +15,13 @@ public class DegreeList {
     public Degree getMajor(String major){
         return null;
     }
+    public Degree getMinor(String minor){
+        return null;
+    }
+    public Degree getTitle(String title) {
+        return null;
+    }
+    public Degree getRequiredCredits(int requiredCredits) {
+        return null;
+    }
 }
