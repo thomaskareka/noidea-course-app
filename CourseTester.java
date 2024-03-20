b4209c4196888ac0be4c445c4e9f51774aa694ce
mearacox <mearacox@gmail.com>
2024-03-19 22:56:39 -0400

    Added tester files

---
+++ CourseTester.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CourseTester {
+	
+	@BeforeClass
+	public void oneTimeSetup() {
+		
+	}
+	
+	@AfterClass
+	public void oneTimeTearDown() {
+		
+	}
+	
+	@BeforeEach
+	public void setup() {
+		//runs before each test
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		//runs after each test
+	}
+}
