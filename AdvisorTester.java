99e41b4c340c5bbdf065ba36db95a367d9486088
Carolina Turner <carolinanturner@gmail.com>
2024-03-24 01:36:55 -0400

    pulling

--- AdvisorTester.java
+++ AdvisorTester.java
@@ -2,77 +2,84 @@ import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class AdvisorTester {
-	
-
-	@BeforeClass
-	public void oneTimeSetup() {
-		
-	}
-	
-	@AfterClass
-	public void oneTimeTearDown() {
-		
-	}
+	private UserList userList = UserList.getInstance();
+	private ArrayList<Student> students = userList.getStudents();
+	private ArrayList<Advisor> advisors = userList.getAdvisors();
+	UUID id = UUID.randomUUID();
 
 	@BeforeEach
 	public void setup() {
 		//runs before each test
+		userList.getAdvisors().clear();
+		userList.getStudents().clear();
+		DataWriter.saveStudents();
+		DataWriter.saveAdvisors();
 
-	
+		userList.addAdvisorUser("AdivsorFriend", "Yay", "email@email.sc.edu", true, "password");
+		userList.addStudentUser("StudentFriend", "Yay", "email@email.sc.edu", "Computer Science", "password");
 	}
 	
 	@AfterEach
 	public void tearDown() {
-		
+		userList.getAdvisors().clear();
+		userList.getStudents().clear();
+		DataWriter.saveStudents();
+		DataWriter.saveAdvisors();
 	}
 
 	//Beginning of testing methods
 
 	@Test
     void testSearchByStudentIDWhenIdExists() {
-       
+        Student result = UserList.getInstance().getStudentFromID(id);
+        assertNotNull(result);
+		assertEquals(id.toString(), result.getStudentID());
     }
 
     @Test
     void testSearchByStudentIDWhenIdDoesNotExist() {
-    
+        Student result = UserList.getInstance().getStudentFromID(id);
+        assertNull(result);
     }
 
     @Test
     void testSearchByStudentIDWhenIdIsNull() {
-      
+		Student result = UserList.getInstance().getStudentFromID(null);
+        assertNull(result);
     }
 
 	@Test 
-	public void testAddAdvisee(){
+	void testAddAdvisee(){
 
 	}
 	@Test 
-	public void testAddCourseForStudent(){
+	void testAddCourseForStudent(){
 
 	}
 	@Test 
-	public void removeCourseForStudent(){
+	void removeCourseForStudent(){
 
 	}
 	@Test 
-	public void testAddNotes(){
+	void testAddNotes(){
 
 	}
 	@Test 
-	public void testEnterFailureRisk(){
+	void testEnterFailureRisk(){
 		
 	}
 	@Test 
-	public void testRemoveFailureRisk(){
+	void testRemoveFailureRisk(){
 
 	}
 	
