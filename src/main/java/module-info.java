147369c4d809132da09744d0f6dd610642127966
awybranski <awybranski@gmail.com>
2024-04-04 12:25:36 -0400

    working on bugs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,9 @@ module noidea {
     opens noidea to javafx.fxml;
     exports noidea;
 
+    opens controllers to javafx.fxml;
+    exports controllers;
+
     opens model to javafx.fxml;
     exports model;
 }
--- src/main/java/noidea/App.java
+++ src/main/java/noidea/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
