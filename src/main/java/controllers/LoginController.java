28651779cd8cf0126f4fa7b8123697658873d107
Thomas Kareka <tkareka@email.sc.edu>
2024-04-07 22:33:11 -0400

    initial login screen, #140

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,5 +1,40 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import noidea.App;
+import model.*;
+
 public class LoginController {
-    
+
+    @FXML
+    private CheckBox menuAdvisorButton;
+
+    @FXML
+    private TextField menuEmailField;
+
+    @FXML
+    private Button menuLoginButton;
+
+    @FXML
+    private PasswordField menuPasswordField;
+
+    @FXML
+    private Button menuSignupButton;
+
+    @FXML
+    void doLogin(ActionEvent event) {
+        System.out.println("Login");
+    }
+
+    @FXML
+    void switchToSignup(ActionEvent event) {
+        System.out.println("Signup");
+    }
+
 }
