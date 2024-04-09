6aa9d0500e84658d14eb0642819476daea54a618
Thomas Kareka <tkareka@email.sc.edu>
2024-04-08 22:39:08 -0400

    #142, add basic pages

--- src/main/java/controllers/CourseListingController.java
+++ src/main/java/controllers/CourseListingController.java
@@ -6,8 +6,10 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -23,13 +25,22 @@ public class CourseListingController implements Initializable {
     @FXML
     private VBox courseBox;
 
+    @FXML
+    private TextField courseField;
+
+    @FXML
+    private Spinner<Integer> pageSpinner;
+
     @FXML @Override
     public void initialize(URL location, ResourceBundle resources) {
-        loadCourses(0);
+        loadCourses(1);
+        pageSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 1000));
+        pageSpinner.valueProperty().addListener((o, oldPage, newPage) -> loadCourses(newPage));
     }
 
     private void loadCourses(int page) {
-        List<Course> foundCourses = App.system.getCoursesFromSearch(page);
+        courseBox.getChildren().clear();
+        List<Course> foundCourses = App.system.getCoursesFromSearch(page - 1);
         for(Course c : foundCourses) {
             Text t = new Text(c.toString());
             t.setWrappingWidth(1200);
