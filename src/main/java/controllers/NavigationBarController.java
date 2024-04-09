b9b380581493c6a90b68ec2b916a3526743bebe5
Thomas Kareka <tkareka@email.sc.edu>
2024-04-09 13:15:55 -0400

    logout and navigation buttons

--- src/main/java/controllers/NavigationBarController.java
+++ src/main/java/controllers/NavigationBarController.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Menu;
@@ -27,11 +29,51 @@ public class NavigationBarController implements Initializable {
         }
     }
 
+    private MenuItem makeButton(String text, String goTo) {
+        MenuItem button = new MenuItem(text);
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle (ActionEvent a) {
+                try {
+                    App.setRoot(goTo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return button;
+    }
+
+    private MenuItem makeLogoutButton() {
+        MenuItem button = new MenuItem("Sign Out");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle (ActionEvent a) {
+                try {
+                    App.system.logout();
+                    App.setRoot("login");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return button;
+    }
+
     private void initStudentMenu() {
-        menuAccount.getItems().add(new MenuItem("Student"));
+        MenuItem noteItem = makeButton("Notes", "notes");
+        MenuItem mapItem = makeButton("Major Map", "majorMap");
+        MenuItem progressItem = makeButton("Degree Progress", "degreeProgress");
+        MenuItem catalogItem = makeButton("Course Catalog", "courseListing");
+        
+        MenuItem profileItem = makeButton("Profile", "studentProfile");
+        MenuItem editItem = makeButton("Edit Profile", "editStudentProfile");
+
+        MenuItem logoutButton = makeLogoutButton();
+
+        menuGoTo.getItems().addAll(noteItem, mapItem, progressItem, catalogItem);
+        menuAccount.getItems().addAll(profileItem, editItem, logoutButton);
     }
 
     private void initAdvisorMenu() {
-        menuAccount.getItems().add(new MenuItem("Advisor"));
+        //TODO
     }
 }
