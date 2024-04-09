59a01f5ea800faa0984a94f528140f2a995493d7
hudsondavis13 <hjd2@email.sc.edu>
2024-04-09 16:32:20 -0400

    worked on #141

--- src/main/java/controllers/StudentProfileController.java
+++ src/main/java/controllers/StudentProfileController.java
@@ -1,6 +1,54 @@
-package controllers;
+    package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class StudentProfileController {
-    
-}
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import model.Student;
+import noidea.App;
+
+public class StudentProfileController implements Initializable {
+        @FXML
+        private TextField firstNameField;
+        @FXML
+        private TextField lastNameField;
+        @FXML
+        private TextField emailField;
+        @FXML
+        private TextField studentIdField;
+        @FXML
+        private TextField majorField;
+        @FXML
+        private TextField minorField;
+        @FXML
+        private TextField classLevelField;
+        @FXML
+        private TextField applicationAreaField;
+        @FXML
+        private TextField majorGPAField;
+        @FXML
+        private TextField overallGPAField;
+
+        @FXML @Override
+        public void initialize(URL location, ResourceBundle resources) {
+            Student student  = App.system.getStudent();
+            if (student != null) {
+                populateStudentProfile(student);
+            }
+        }
+
+        private void populateStudentProfile(Student student) {
+            firstNameField.setText(student.getFirstName());
+            lastNameField.setText(student.getLastName());
+            emailField.setText(student.getEmail());
+            studentIdField.setText(student.getStudentID());
+            majorField.setText(student.getMajor());
+            minorField.setText(student.getMinor());
+            classLevelField.setText(student.getClassLevel());
+            applicationAreaField.setText(student.getApplicationArea());
+            majorGPAField.setText(String.valueOf(student.getMajorGPA()));
+            overallGPAField.setText(String.valueOf(student.getOverallGPA()));
+        }
+    }
