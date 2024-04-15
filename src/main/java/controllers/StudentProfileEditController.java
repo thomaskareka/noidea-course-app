68359dc164fabe983cfb4d268aa1b4f3bfce46b9
mearacox <mearacox@gmail.com>
2024-04-14 20:12:04 -0400

    Student profile edit controller

---
+++ src/main/java/controllers/StudentProfileEditController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class StudentProfileEditController {
+
+    @FXML
+    private TextField applicationAreaField;
+
+    @FXML
+    private TextField classLevelField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField majorField;
+
+    @FXML
+    private TextField minorField;
+
+    @FXML
+    private Button saveBtn;
+
+    @FXML
+    private TextField studentIDField;
+
+    @FXML
+    void saveBtnPressed(ActionEvent event) {
+        
+    }
+
+}
+
