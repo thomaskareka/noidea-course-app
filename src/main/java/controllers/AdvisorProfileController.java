31f0ef08ee50e81190992ffa7852c54ffed2a178
Carolina Turner <carolinanturner@gmail.com>
2024-04-16 12:54:39 -0400

    added advisor edi tprofile

---
+++ src/main/java/controllers/AdvisorProfileController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import noidea.App;
+import model.*;
+
+public class AdvisorProfileController implements Initializable {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+}
